f366ea7ba1586b566193edb58522af96e580b3c0
RobinDhami <rozendhami69@gmail.com>
2024-04-13 22:42:45 +0545

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,19 @@
 database = {}
 
-name = input("Enter your name")
-location = input("Enter your location")
-database = {"name":name,"location":location}
+name = input("Enter your name: ")
+location = input("Enter your location: ")
+
+#Updating Dictionary
+database["name"] = name
+database["location"] = location
+
 print(database)
+
+a = input("Enter your name to check permission: ")
+b = input("Enter your location to check permission: ")
+
+# Check if the entered name and location match the database
+if name == a and location == b:
+    print("Permission Granted")
+else:
+    print("Permission Denied")
