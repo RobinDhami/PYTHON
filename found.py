854949a887a80077a07200c6ddb743a80a04ba1d
RobinDhami <rozendhami69@gmail.com>
2024-04-14 19:41:58 +0545

    Palindrome

---
+++ found.py
@@ -0,0 +1,13 @@
+a = "Hello my name is rabin Dhami.I am a boy"
+
+if "rabin"  in a:
+    print("Rabin present")
+
+#Check for Palindrome    
+
+word = "..........Damad,"
+word = word.lower().replace(" ","").replace(",","").replace(".","")
+palin = word[::-1]
+
+if(word==palin):
+    print("It is a palindrome")
