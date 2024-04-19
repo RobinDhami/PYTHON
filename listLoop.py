97a612602970dbb8f820af2cb343e4b4ce61340b
RobinDhami <rozendhami69@gmail.com>
2024-04-19 19:11:19 +0545

    loopList

---
+++ listLoop.py
@@ -0,0 +1,18 @@
+#Loop in the list 
+
+list = ["apple", "ball" , "cate" , "dog" , "rat"]
+
+for x in list:
+    print(x)
+
+for i in range(len(list)):
+    print(i ,list[i])
+
+#Using while Loop:
+i=0
+while i<len(list):
+    print(i)
+    i = i+1
+
+#Looping thorugh list
+[print(f" this is {x}")  for x in  list]    
