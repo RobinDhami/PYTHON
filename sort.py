77f311c135687740a3bcde6ba190b6f7dca252cc
RobinDhami <rozendhami69@gmail.com>
2024-04-21 22:25:49 +0545

    copy

--- sort.py
+++ sort.py
@@ -1,6 +1,18 @@
 list = ["apple", "zebra" , "cat" ," ball"]
+a = list.copy()
+print(a)
 list.sort()
 print(list)
 
 list.sort(reverse=True)
 print(list)
+
+
+#Palindrome 
+a=input("Enter a word")
+a=a.lower().replace(".","").replace(",","").replace(" ","")
+b=a[::-1]
+if (a==b):
+    print("Palindrome")
+else:
+    print("Not palindrome")    
