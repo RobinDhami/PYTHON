345f0aaeebcf4c89047aaa9d7021b40e305b5b49
RobinDhami <rozendhami69@gmail.com>
2024-05-07 07:43:42 +0545

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,19 +1,54 @@
-database = {}
+# database = {}
 
-name = input("Enter your name: ")
-location = input("Enter your location: ")
+# name = input("Enter your name: ")
+# location = input("Enter your location: ")
 
-#Updating Dictionary
-database["name"] = name
-database["location"] = location
+# #Updating Dictionary
+# database["name"] = name
+# database["location"] = location
 
-print(database)
+# print(database)
 
-a = input("Enter your name to check permission: ")
-b = input("Enter your location to check permission: ")
+# a = input("Enter your name to check permission: ")
+# b = input("Enter your location to check permission: ")
 
-# Check if the entered name and location match the database
-if name == a and location == b:
-    print("Permission Granted")
-else:
-    print("Permission Denied")
+# # Check if the entered name and location match the database
+# if name == a and location == b:
+#     print("Permission Granted")
+# else:
+#     print("Permission Denied")
+
+
+
+# Methods for dictionary 
+
+dictionry = {
+"car" :"tesla",
+"bike" : "xpulse"
+}
+
+# Get specific value for a key 
+x=dictionry.get("car")
+print(x)
+
+# add value to lasaat index
+dictionry["rabin"] = "dhami"
+print(dictionry)
+
+dictionry.update({ "summer":"hot"})
+# Get list if keys 
+y=dictionry.keys()
+print(y)
+
+# Get list of values 
+z = dictionry.values()
+print(z)
+
+a = dictionry.items()
+print(a)
+
+for i in a:
+    if i == ("car","raa"):
+        print(i)
+    else:
+        print("not found")    
