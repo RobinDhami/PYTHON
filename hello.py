40f8674ff5070508e83545343f76b607af685ea2
Robin Dhami <rozendhami69@gmail.com>
2024-05-15 19:59:05 +0545

    enumerate

--- hello.py
+++ hello.py
@@ -1,15 +1,4 @@
-okno commit python presentation 
-print("hello world")
-no commit today 
-hey siri
-laptop repair part 2
-3rd commit
-isp crisis may 2
-salary tomorrow 😸 
-may 3rd
+
 kwargs learned
-ram bish
-python 3
-no commit?
-may 10 
 hostel day 1
+loops and enumerate learned 
