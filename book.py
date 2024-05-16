3e65b2e028b629e5812bbaa6e7923c6b22d5d8d5
RobinDhami <rozendhami69@gmail.com>
2024-05-16 21:49:03 +0545

    new boook

---
+++ book.py
@@ -0,0 +1,22 @@
+# #palindrome 
+
+# n=int(input("Enter a number"))
+# print(n)
+# while (n!=0):
+#     reverse = 0
+#     remainder = n%10
+#     reverse = abs(reverse*10 + remainder)
+#     n = n/10
+#     print(n)
+#     print(reverse)    
+#     if(reverse==n):
+#         print('palindrome')
+     
+#     else:
+#         print("Not palindrome")    
+  
+
+a = 42 
+print(id(a))
+a = 43
+print(id(a))
--- enumerate.py
+++ enumerate.py
@@ -1,8 +1,22 @@
-fruits = ["apple", "ball", "cat" , "dog"]
+# fruits = ["apple", "ball", "cat" , "dog"]
 
-for i,x in enumerate(fruits):
-    print(i,x)
+# for i,x in enumerate(fruits):
+#     print(i,x)
 
-import math 
-a=math.sqrt(64)
-print(a)
+# import math 
+# a=math.sqrt(64)
+# print(a)
+# s=pow(4,3)
+# min(2,3,1)
+# max(9,3,10)
+# abs(-90)
+
+# r=2
+# p=math.pi
+# area = p * r*r
+# print(area)
+# print(s)
+# import datetime
+# b= datetime.datetime.now()
+# print(b)
+
