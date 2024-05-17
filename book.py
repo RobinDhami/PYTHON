5ac28ecc6e98b0bbb46b9be2e31e1650c7817415
RobinDhami <rozendhami69@gmail.com>
2024-05-17 11:43:26 +0545

    deep learning

--- book.py
+++ book.py
@@ -30,4 +30,37 @@ fruits = "apple"
 print(id(fruits))
 fruits="ball"
 print(id(fruits))
-   
+
+code = "I am a boy"
+r = code.encode('utf-8')
+print(r)
+print(type(r))
+
+tup = (3,4,2,0,5,7,9) # n  Immutable
+print(tup) 
+
+lis = [1,4,0,3,2,9]
+lis.sort()
+print(lis)
+x= max(lis)
+print(x)
+print(lis[2:4])
+
+l = list(range(1,6))
+print(type(l))
+
+dic =dict(zip("hello",range(1,6)))
+print(dic)
+
+database = {}
+database.update({"rabin":"dhami", "school":"grade"})
+database["rabin "]= "dhami"
+print(database)
+name = input("Enter your name")
+password = input("Enter Password")
+if name in database and database[name]==password:
+    print("Logged in successfully")
+elif name in database or name not in database:
+    print("Invalid name or password")
+else:
+    print("No such account . Create a login account")    
