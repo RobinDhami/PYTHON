f8bfcb780f8825da13730ae217ffacccfb3842e0
RobinDhami <rozendhami69@gmail.com>
2024-05-20 09:37:19 +0545

    prime class

--- prime.py
+++ prime.py
@@ -1,26 +1,32 @@
 primes = []  # this will contain the primes in the end
 upto = 100  # the limit, inclusive
 for n in range(2, upto + 1):
-   is_prime = True  # flag, new at each iteration of outer for
    for divisor in range(2, n):
       if n % divisor == 0:
-         is_prime = False
          break
-   if is_prime:  # check on flag
-         primes.append(n)
+   else: 
+      primes.append(n)
 print(primes)
 
 
-
-class prime:
-   prime=[]
-   def prime(n):
+class Prime:
+   def calc_prime(self,n):
+      primes=[]
       for i in range(2,n+1):
-         is_prime=True
-         for x in range(2,n):
+         for x in range(2,i):
             if i%x==0:
-               is_prime=False
                break
-         if is_prime:
-            prime.append(i)
-   print(prime)                
+         else:
+            primes.append(i)      
+      return primes   
+
+result = Prime()
+x=result.calc_prime(100) 
+print(x)                
+
+# x = [1, 2, 3] #global 
+# def func(x):
+#    x[1] = 42  # this changes the caller!
+#    x = 'something else'  # this points x to a new string object but is local cant access outside the func
+# func(x)
+# print(x)
