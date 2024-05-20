240ce4d33487c94991c98ba190dee44f17b80822
RobinDhami <rozendhami69@gmail.com>
2024-05-20 21:11:03 +0545

    kwargs

---
+++ func.py
@@ -0,0 +1,16 @@
+# def func(**a):
+#     print(a)
+
+# func(a=2,b=6)
+
+
+import mysql.connector
+
+
+db = mysql.connector.connect(
+    'host': 'localhost',     
+    'username': '',
+    'password': 'RAB@@123'
+)
+
+
