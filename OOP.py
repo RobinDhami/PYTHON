0b39f6296fda806ab4f57c83311b42d3abbbc8f1
RobinDhami <rozendhami69@gmail.com>
2024-06-23 20:52:09 +0545

    polymorphis

--- OOP.py
+++ OOP.py
@@ -1,43 +1,43 @@
-class simple():
-    pass
+# class simple():
+#     pass
 
-print(type(simple))
+# print(type(simple))
 
-simp = simple()
-print(type(simp)==simple()) #Here it checking if a object is equal to class type so false
-print(type(simp)==simple) 
+# simp = simple()
+# print(type(simp)==simple()) #Here it checking if a object is equal to class type so false
+# print(type(simp)==simple) 
 
-class Point():
-    x=10
-    y=12
+# class Point():
+#     x=10
+#     y=12
 
-p =Point()
-del p.x
-print(p.x)  #This will throw an error cause we deleted this instance
-print(p.y)
+# p =Point()
+# del p.x
+# print(p.x)  #This will throw an error cause we deleted this instance
+# print(p.y)
 
-p.x=100
-print(p.x)
+# p.x=100
+# print(p.x)
 
-class Area():
-    def square(self,side):
-        area=side**2 
-        print(area)   
+# class Area():
+#     def square(self,side):
+#         area=side**2 
+#         print(area)   
 
-a =Area()
-l = int(input("Enter the length of sqaure"))
-a.square(l)
+# a =Area()
+# l = int(input("Enter the length of sqaure"))
+# a.square(l)
 
-class Rectangle:
-    def __init__(self,a,b): 
-        self.a=a
-        self.b=b
-    def area(self):
-        ar=self.a*self.b
-        print(ar)
+# class Rectangle:
+#     def __init__(self,a,b): 
+#         self.a=a
+#         self.b=b
+#     def area(self):
+#         ar=self.a*self.b
+#         print(ar)
         
-r=Rectangle(23,4)
-r.area()        
+# r=Rectangle(23,4)
+# r.area()        
 
 # Define the base class 'vehicle'
 class Vehicle:
@@ -61,4 +61,33 @@ if __name__ == "__main__":
     print(f"Engine: {my_tesla.engine}")
     print(f"Autopilot: {my_tesla.autopilot}")
 
-    
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Drive!")
+
+class Boat:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Sail!")
+
+class Plane:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang")       #Create a Car class
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat class
+plane1 = Plane("Boeing", "747")     #Create a Plane class
+
+for x in (car1, boat1, plane1):
+  x.move()    
