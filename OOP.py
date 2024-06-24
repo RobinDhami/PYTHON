a9a1ba8e4c7be00b393e8160e6531c986a8f5ef1
RobinDhami <rozendhami69@gmail.com>
2024-06-24 22:32:59 +0545

    inheritance class polymorphism

--- OOP.py
+++ OOP.py
@@ -39,6 +39,7 @@
 # r=Rectangle(23,4)
 # r.area()        
 
+
 # Define the base class 'vehicle'
 class Vehicle:
     def __init__(self, color, engine):
@@ -91,3 +92,32 @@ plane1 = Plane("Boeing", "747")     #Create a Plane class
 
 for x in (car1, boat1, plane1):
   x.move()    
+
+
+class Vehicle:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Move!")
+
+class Car(Vehicle):
+  pass
+
+class Boat(Vehicle):
+  def move(self):
+    print("Sail!")
+
+class Plane(Vehicle):
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang") #Create a Car object
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+plane1 = Plane("Boeing", "747") #Create a Plane object
+
+for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
+  x.move()
