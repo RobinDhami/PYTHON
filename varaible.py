92d15756d523046995eb6c44d33939dd865901fe
RobinDhami <rozendhami69@gmail.com>
2024-08-07 21:49:08 +0545

    local and global variable

--- varaible.py
+++ varaible.py
@@ -1,7 +1,12 @@
 x="rabin"
+y=10
 def hello():
+    global y
+    y=45
     x="dhami"
     print(f"this is local variable{x}")
 
 print(f" this is global variable {x}")
-hello()    
+print(y)
+hello()    #After the function hello is called the value of y changes to 45!!
+print(y)
