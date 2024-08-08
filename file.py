5ffb6439e7d0dde296f1b37421fad5c791f67a27
RobinDhami <rozendhami69@gmail.com>
2024-08-08 21:51:10 +0545

    file handling

--- file.py
+++ file.py
@@ -8,12 +8,15 @@ f.close()
 # f.write("Hello world")
 # f.close
 
-f=open("list.txt",'a') #Append just add data to the existing file data
+f=open("text.txt",'a') #Append just add data to the existing file data
 f.write("Hello world")
+f.truncate(5)          #Describes the size of the file . here the size of file is 5byte   
 f.close
 
 with open("list.txt",'a') as f: #In this case we dont  need to add a f.close() it qutomatically the file everytime
     f.write("This is good")
+    f.truncate(100)
+
 
 f = open("dsa.py",'r')
 while True:
@@ -21,6 +24,9 @@ while True:
     if not line:
         break
     print(line)
+f.seek(10)    #Moves the cursor to certain postion and the read start from the seek position
+z=f.tell()      #Tell will tell us how much we have seeked
+print(z)
 data = f.read(5) 
 print(data)   
 
