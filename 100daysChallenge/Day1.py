e087241689d33edaa8df175c76cf67b427d17ef6
RobinDhami <rozendhami69@gmail.com>
2024-09-08 22:24:28 +0545

    enumerate

--- 100daysChallenge/Day1.py
+++ 100daysChallenge/Day1.py
@@ -8,3 +8,20 @@ for index,value in enumerate(games):
     print(len(games)-index-1,value)
 
 print(dict(list(enumerate(games))))
+
+d={"abhijeet":9, "Mrinali":10,"kalu":7 , "samriddha":6 , "robin":"BSc CSIT"}
+
+for i,key in enumerate(d):
+    value=d[key]
+    print(i,key,value)
+
+input_list = ["apple", "banana", "apple", "orange", "banana", "pear"]
+
+output_list=[]
+for i,value in enumerate(input_list):
+    if value not in output_list:
+        output_list.append(value)
+
+print(output_list)    
+
+#Using set
