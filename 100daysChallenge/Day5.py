2d9a281c0ad9d480292e0f817c4577de5c7af8e9
RobinDhami <rozendhami69@gmail.com>
2024-09-20 21:53:35 +0545

    set

--- 100daysChallenge/Day5.py
+++ 100daysChallenge/Day5.py
@@ -1,3 +1,10 @@
-l =["helooo" ,"i" ,"am" ,"a" ,"boy"]
-r=l.sort()
-print(r)
+l =[34,67,1,90,0,54]
+print(max(l))
+
+s = "Rabin Dhami"
+s1 = list(s)
+st=set()
+for i in s1:
+    if i!=" ":
+        st.add(i)    
+print(list(st))
