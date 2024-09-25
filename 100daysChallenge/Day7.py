9c0e779749eae57888777464e864411973d1d7a2
RobinDhami <rozendhami69@gmail.com>
2024-09-25 21:19:54 +0545

    filter

--- 100daysChallenge/Day7.py
+++ 100daysChallenge/Day7.py
@@ -1,3 +1,4 @@
+from functools import reduce
 #Lambda Function
 
 l=[1,2,3,4,5,6]
@@ -5,3 +6,9 @@ l=[1,2,3,4,5,6]
 print(list(map(lambda a:a*a,l)))
 res=filter(lambda a: a%2==0 ,l )
 print(list(res))
+
+b=[2,3,4]
+squared = map(lambda a:a*a,l)
+rem=filter(lambda a:a<10,squared)
+print(list(rem))
+print(reduce(lambda a,b:a+b,l))
