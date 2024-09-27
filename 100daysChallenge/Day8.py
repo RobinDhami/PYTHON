7463ffadfe34e28d1fcc32376c6aac7ec9f0d29e
RobinDhami <rozendhami69@gmail.com>
2024-09-27 22:45:10 +0545

    custom map

--- 100daysChallenge/Day8.py
+++ 100daysChallenge/Day8.py
@@ -3,7 +3,20 @@
 l=[2,3,4,5]
 print(list(map(lambda x:x*x,l)))
 
-#Filter
-
+#Filter 
 print(list(filter(lambda x:x<=3,l)))
 
+#Custom map function
+
+def cumap(func,itr):
+    res=[]
+    for i in itr:
+        res.append(func(i))
+    return res   
+
+def min(a):
+    return a-1
+
+lis=[2,3,4,5,6]
+result=cumap(min,lis)
+print(result)
